e0dee1f42c9b37b0fff5531c432ea312c92ded34
darco2018 <newsecond@outlook.com>
2019-05-31 18:09:28 +0200

    Show/hide trash on hover

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 /** @format */
 
 import "../css/main.scss";
+
+$(document).ready(function main() {
+  const $plusIcon = $(".fa-plus-circle");
+  const $newTodoInput = $(".new-todo");
+  const $todolist = $(".list");
+  const $todoItems = $(".list__item");
+  const $trashIcon = $(".fa-trash");
+  const trashIconClass = "fa-trash";
+  const trashIcon = `<i class='fa ${trashIconClass}'></i>`;
+
+  // we define in 2nd arg:'this' (note: we skip $())
+  $todolist.on("mouseenter", ".list__item", function showTrash() {
+    // console.log($(this).text());
+    $(this).prepend(trashIcon);
+  });
+
+  $todolist.on("mouseleave", ".list__item", function hideTrash() {
+    $(this)
+      .find(`.${trashIconClass}`)
+      .remove();
+  });
+});
